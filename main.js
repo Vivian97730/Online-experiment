728c42da660a1f95510015554c6526ad1ca9443e
Vivian97730 <vivian97730@gmail.com>
2021-06-26 13:51:17 +0800

    modified

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ function route(path){
 }
 
 function getProduct(productCategory, id, source){
-    localStorage.setItem('viewProductCate', productCategory);
-    localStorage.setItem('viewProductId', id);
-    if(source != 'recommend'){
-      localStorage.setItem('viewSource', source);
-    }else{
-      localStorage.setItem('source', source);
-    }
-    window.location.href = 'productIntro.html?productCategory='+ productCategory +'&id='+ id;
+  // localStorage.setItem('viewProductCate', productCategory);
+  // localStorage.setItem('viewProductId', id);
+  if(source == 'preference'){
+    localStorage.setItem('fromPreference', 1);
+  }else if(source == 'allProduct'){
+    localStorage.setItem('fromAllProduct', 1);
+  }else if(source == 'category'){
+    localStorage.setItem('fromCategory', 1);
+  }else if(source == 'hotProduct'){
+    localStorage.setItem('fromHotProduct', 1);
+  }else if(source == 'recommend'){
+    localStorage.setItem('fromRecommend', 1);
   }
+  window.location.href = 'productIntro.html?productCategory='+ productCategory +'&id='+ id;
+}
 
-  function getCategory(category){
-    localStorage.setItem('categoryIntro', category);
-    window.location.href = 'category.html?category='+ category;      
-  }
+function getCategory(category){
+  // localStorage.setItem('categoryIntro', category);
+  window.location.href = 'category.html?category='+ category;      
+}
